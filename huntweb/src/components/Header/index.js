b1460de1171377586dc8c43b4d9d11ccb72663b5
Danilo Cáceres Tanaka <danilo.tanaka@tivit.com>
2020-08-15 15:42:57 -0300

    Create Hello Header Component

---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// Using a stateless component the code bellow show it like an object.
+
+// class Header extends Component {
+// 	render() {
+//    return <h1>Hello</h1>
+// 	}
+// }
+
+const Header = () = (
+  <h1>Hello</h1>
+);
+
+export default
