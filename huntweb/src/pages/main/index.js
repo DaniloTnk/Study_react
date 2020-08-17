b0b6ddd3f0b1c3ee28b9c60233d6d70ec0b2d156
Danilo Cáceres Tanaka <danilo.tanaka@tivit.com>
2020-08-17 14:10:54 -0300

    Include routes on project, create product routes and create product pages.

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from "../../services/api";
-import "./styles.css"
+
+import { Link } from "react-router-dom";
+import './styles.css';
 
 export default class Main extends Component {
   state = {
@@ -22,28 +24,28 @@ export default class Main extends Component {
   };
 
   prevPage = () => {
-    const { page, productInfo } = this.state;
+    const { page } = this.state;
 
     if (page === 1) return;
 
     const pageNumber = page - 1;
-
     this.loadProducts(pageNumber);
-  };
+
+  } 
 
   nextPage = () => {
     const { page, productInfo } = this.state;
 
-    if (page === productInfo.pages ) return;
+    if (page === productInfo.pages) return;
 
     const pageNumber = page + 1;
-
     this.loadProducts(pageNumber);
-  };
+  }
 
   render() {
-    const { products, productInfo, page }= this.state
 
+    const { products, page, productInfo } = this.state;
+    
     return (
       <div className="product-list">
         {products.map(product => (
@@ -51,12 +53,16 @@ export default class Main extends Component {
             <strong>{product.title}</strong>
             <p>{product.description}</p>
 
-            <a href="/">Acessar</a>
+            <Link to={`/products/${product._id}`}>Acessar</Link>
           </article>
         ))}
         <div className="actions">
-          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
-          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+          <button disabled={page === 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>
+            Próxima
+          </button>
         </div>
       </div>
       );
